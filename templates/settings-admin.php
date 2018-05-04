7a6226b3588fa9934ac7014be5d9bffbd1d30984
Claus Zinn <claus.zinn@uni-tuebingen.de>
2018-05-04 10:55:29 +0200

    Changed string from EUDAT to CLARIN in settings

--- templates/settings-admin.php
+++ templates/settings-admin.php
@@ -1,5 +1,5 @@
 <div class="section" id="eudat_lrswitchboard">
-    <h2>EUDAT LRSWITCHBOARD Bridge</h2>
+    <h2>CLARIN LRSWITCHBOARD Bridge</h2>
     <p id="lrswitchboardUrlField">
         <input title="switchboard_baseurl" type="text" name="switchboard_baseurl"
                id="lrswitchboardUrl"
