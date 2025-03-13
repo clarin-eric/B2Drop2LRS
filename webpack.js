7fe8654bd13c7d82c5a29c48317c575e8921481c
André Moreira <amoreira.mail@gmail.com>
2025-03-13 16:27:43 +0100

    Update version for github tag
    Using webpack-shell-plugin-next plugin

--- webpack.js
+++ webpack.js
@@ -1,3 +1,24 @@
+const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 const webpackConfig = require('@nextcloud/webpack-vue-config')
+
+webpackConfig.plugins.push(
+    new WebpackShellPluginNext({
+      onBuildStart:{
+        scripts: [
+            'VERSION="${GITHUB_REF##*/}";\
+            if [ -n "VERSION" ];\
+            then\
+                echo "Github ref exists -> updating App version number to: "${VERSION}"";\
+                NEW_INFO=$(sed -E "s/<version>[0-9]{1,3}\.[0-9]{1,3}(\.[0-9]{1,3})?<\\/version>/<version>${VERSION}<\\/version>/g" appinfo/info.xml);\
+                echo "$NEW_INFO" > appinfo/info.xml;\
+            else\
+                echo "No Github ref -> using HEAD version";\
+            fi'
+        ],
+        blocking: true,
+        parallel: false
+      }
+    })
+) 
 
 module.exports = webpackConfig
