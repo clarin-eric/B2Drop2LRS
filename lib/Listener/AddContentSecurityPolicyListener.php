73fadac5dfd7fccca3bd0db2d0d7736dcb63d971
André Moreira <amoreira.mail@gmail.com>
2025-03-21 18:41:53 +0100

    Add PHP doc to AddContentSecurityPolicyListener.php

--- lib/Listener/AddContentSecurityPolicyListener.php
+++ lib/Listener/AddContentSecurityPolicyListener.php
@@ -21,10 +21,26 @@ use OCP\EventDispatcher\IEventListener;
 use OCP\Security\CSP\AddContentSecurityPolicyEvent;
 
 /**
- * @template-implements IEventListener<Event>
+ * Implementation for AddContentSecurityPolicyListener
+ * This listener adds 'https://switchboard.clarin.eu/' to the form-action
+ * allowed domains of NC CSP.
+ * This is needed in order to POST data from NC to the Switchboard
+ *
+ * @category  Nextcloud
+ * @package   SwitchboardBridge
+ * @author    André Moreira <andre@clarin.eu>
+ * @license   https://github.com/clarin-eric/B2Drop2LRS/blob/master/LICENSE AGPL v3
+ * @link      https://github.com/clarin-eric/B2Drop2LRS
  */
 class AddContentSecurityPolicyListener implements IEventListener {
 
+    /**
+     * Handle event
+     * 
+     * @param Event $event The triggering event
+     *
+     * @return void
+     */
     public function handle(Event $event): void {
         if (!($event instanceof AddContentSecurityPolicyEvent)) {
             return;
