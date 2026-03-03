d2e335b66e981d8c5c306d8c2bd404f214d22f34
André Moreira <amoreira.mail@gmail.com>
2026-03-03 14:04:04 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,15 @@ module.exports = {
 	extends: [
 		'@nextcloud',
 	],
-    settings: {
-        'import/resolver': {
-            'custom-alias': {
-                alias: {
-                    '@nextcloud/files-v4': './node_modules/@nextcloud/files-v4'
-                },
-                extensions: ['.js', '.vue']
-            }
-        }
-    }
+	settings: {
+		'import/resolver': {
+			'custom-alias': {
+				alias: {
+					'@nextcloud/files-v4': './node_modules/@nextcloud/files-v4'
+				},
+				onlyModule: ['@nextcloud/files-v4'],
+				extensions: ['.js', '.vue']
+			}
+		}
+	}
 }
